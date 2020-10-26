90dca9a5cd7df660a0c96f014032b533010de23e
cheolwon1994 <chlcjfahs@naver.com>
2020-10-26 11:35:40 +0900

    Finish Memo App

--- Memo/ComposeViewController.m
+++ Memo/ComposeViewController.m
@@ -14,15 +14,42 @@
 - (IBAction)close:(id)sender;
 - (IBAction)save:(id)sender;
 @property (weak, nonatomic) IBOutlet UITextView *memoTextView;
-
+@property (strong, nonatomic) id willShowToken;
+@property (strong, nonatomic) id willHideToken;
 
 @end
 
 @implementation ComposeViewController
 
+-(void) dealloc {
+    if(self.willShowToken){
+        [[NSNotificationCenter defaultCenter] removeObserver:self.willShowToken];
+    }
+    if(self.willHideToken){
+        [[NSNotificationCenter defaultCenter] removeObserver:self.willHideToken];
+    }
+}
+
+/*
+ 이전 설정 유지
+ 자판이 띄어진 상태에서 뒤로가기를 눌렀다면 재접속시 자판이 자동으로 띄어짐
+ 자판이 띄어지지 않은 상태에서 뒤로가기를 눌렀다면 재접속시 자판이 자동으로 띄어지지 않음
+ */
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.memoTextView becomeFirstResponder];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self.memoTextView resignFirstResponder];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //메모 편집과 새 메모 작성을 구분하기 위한 코드
     if(self.editTarget != nil){
         self.navigationItem.title = @"메모 편집";
         self.memoTextView.text = self.editTarget.content;
@@ -31,6 +58,30 @@
         self.navigationItem.title = @"새 메모";
         self.memoTextView.text = @"";
     }
+    
+    //자판을 켰을 때, 글을 가리지 않도록 설정
+    self.willShowToken = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        CGFloat height = [note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].size.height;
+        UIEdgeInsets inset = self.memoTextView.contentInset;
+        inset.bottom = height;
+        self.memoTextView.contentInset = inset;
+        
+        inset = self.memoTextView.scrollIndicatorInsets;
+        inset.bottom = height;
+        self.memoTextView.scrollIndicatorInsets = inset;
+    }];
+    
+    //자판 해제
+    self.willHideToken = [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        
+        UIEdgeInsets inset = self.memoTextView.contentInset;
+        inset.bottom = 0;
+        self.memoTextView.contentInset = inset;
+        
+        inset = self.memoTextView.scrollIndicatorInsets;
+        inset.bottom = 0;
+        self.memoTextView.scrollIndicatorInsets = inset;
+    }];
 }
 
 /*-
